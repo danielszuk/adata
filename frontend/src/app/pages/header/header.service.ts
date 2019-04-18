969e8101eafecc2c134de5f2a1f43aa71b836328
daniel.szuk <daniel.szuk@codelise.io>
2019-04-18 17:19:19 +0200

    fix(header): ui issues

--- frontend/src/app/pages/header/header.service.ts
+++ frontend/src/app/pages/header/header.service.ts
@@ -1,6 +1,5 @@
 import { Injectable, OnDestroy } from '@angular/core';
 import { Router, RoutesRecognized } from '@angular/router';
-import { Location } from '@angular/common';
 import { Subscription } from 'rxjs';
 
 @Injectable({
@@ -10,21 +9,17 @@ export class HeaderService implements OnDestroy {
   private hidden;
   private routerSubscription: Subscription;
 
-  constructor(
-    private readonly router: Router,
-    private readonly location: Location
-  ) {
+  constructor(private readonly router: Router) {
     // Make header visible defaultly for every route (if a route set hidden, the next route set back)
-    let lastPath = this.location.path();
+    let lastPath;
     this.routerSubscription = this.router.events.subscribe(ev => {
       if (ev instanceof RoutesRecognized) {
-        const currentPath = this.location.path();
-        if (currentPath !== lastPath) {
-          if (this.hidden) {
-            this.hidden = false;
-          }
-          lastPath = currentPath;
+        const currentPath = ev.urlAfterRedirects;
+        if (currentPath !== lastPath && this.hidden) {
+          this.hidden = false;
         }
+
+        lastPath = currentPath;
       }
     });
   }
