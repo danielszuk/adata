fc46afc8953a27fed39b3252a06edf3c9b00cd23
daniel.szuk <daniel.szuk@codelise.io>
2019-04-18 10:50:42 +0200

    fix(auth): expression changed after it was checked error when logging in

--- frontend/src/app/pages/header/header.service.ts
+++ frontend/src/app/pages/header/header.service.ts
@@ -1,12 +1,38 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
+import { Router, RoutesRecognized } from '@angular/router';
+import { Location } from '@angular/common';
+import { Subscription } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class HeaderService {
+export class HeaderService implements OnDestroy {
   private hidden;
+  private routerSubscription: Subscription;
 
-  constructor() {}
+  constructor(
+    private readonly router: Router,
+    private readonly location: Location
+  ) {
+    // Make header visible defaultly for every route (if a route set hidden, the next route set back)
+    const lastPath = this.location.path();
+    this.routerSubscription = this.router.events.subscribe(ev => {
+      if (ev instanceof RoutesRecognized) {
+        const currentPath = this.location.path();
+        if (currentPath !== lastPath) {
+          if (this.hidden) {
+            this.hidden = false;
+          }
+        }
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.routerSubscription) {
+      this.routerSubscription.unsubscribe();
+    }
+  }
 
   public get visible() {
     return !this.hidden;
