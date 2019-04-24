e6ee51933782e0b2b22f4851f5a19a68a9fd3f4e
daniel.szuk <daniel.szuk@codelise.io>
2019-04-24 16:38:26 +0200

    feat(orm): remove ormconfig.js

--- backend/src/apps/api/api.module.ts
+++ backend/src/apps/api/api.module.ts
@@ -7,9 +7,19 @@ import { DataBankWorldBankModule } from '../../modules/databank-worldbank/databa
 import { MatchModule } from '../../modules/match/match.module';
 import { VisualizationModule } from '../../modules/visualization/visualization.module';
 import { UserModule } from '../../modules/user/user.module';
+import { Env } from '../../modules/util/env/variables';
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: Env.POSTGRES_HOST,
+      port: Env.POSTGRES_PORT,
+      username: Env.POSTGRES_USER,
+      password: Env.POSTGRES_PASSWORD,
+      database: Env.POSTGRES_DB,
+      entities: ['../../**/**.entity{.ts,.js}'],
+      synchronize: true,
+    }),
     MatrixModule,
     DimensionModule,
     DataBankWorldBankModule,
