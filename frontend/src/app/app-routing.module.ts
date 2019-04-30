cc14525e15ef2506b75c73a3a0dade0c83a095df
daniel.szuk <daniel.szuk@codelise.io>
2019-04-30 13:56:27 +0200

    refactor: modules

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from 'src/app/pages/page-not-found/page-not-found.component';
 import { AdminAuthGuard } from './common/modules/auth/guards/admin-auth-guard.service';
-import { AuthGuard } from './common/modules/auth/guards/auth-guard.service';
 import { AuthCallbackComponent } from './common/modules/auth/auth-callback/auth-callback.component';
+import { VisualizationBrowserComponent } from './pages/visualization-browser/visualization-browser.component';
+import { VisualizationSavedComponent } from './pages/visualization-browser/visualization-saved/visualization-saved.component';
+import { VisualizationDetailsComponent } from './pages/visualization-details/visualization-details.component';
+// tslint:disable-next-line:max-line-length
+import { VisualizationDetailsFullScreenComponent } from './pages/visualization-details/visualization-details-full-screen/visualization-details-full-screen.component';
 
 const routes: Routes = [
-  {
-    path: 'browser',
-    loadChildren:
-      'src/app/pages/visualization-browser/visualization-browser.module#VisualizationBrowserModule'
-  },
+  { path: 'my-visualizations', component: VisualizationSavedComponent },
+  { path: 'browser/:search', component: VisualizationBrowserComponent },
+  { path: 'browser', component: VisualizationBrowserComponent },
   {
     path: 'visualization',
     loadChildren:
@@ -22,10 +24,10 @@ const routes: Routes = [
       'src/app/pages/sync-config/sync-config.module#SyncConfigModule',
     canActivate: [AdminAuthGuard]
   },
+  { path: 'visualization/:id', component: VisualizationDetailsComponent },
   {
-    path: 'visualization',
-    loadChildren:
-      'src/app/pages/visualization-details/visualization-details.module#VisualizationDetailsModule'
+    path: 'visualization/:id/full-screen',
+    component: VisualizationDetailsFullScreenComponent
   },
   {
     path: 'auth/callback/:jwt',
