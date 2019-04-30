a06b0e963d972a4748d44602e495eff756777498
pal.kollar <pal.kollar@codelise.io>
2019-04-30 12:27:42 +0200

    fix: console.log removed

--- backend/src/modules/util/typeorm/pagination.ts
+++ backend/src/modules/util/typeorm/pagination.ts
@@ -3,7 +3,6 @@ import { Env } from '../env/variables';
 export function GetPagination(query): Pagination {
   let skip: number = 0;
   const take = Env.API_DEFAULT_PAGE_LIMIT;
-  console.log();
 
   const parsedPage: number = parseInt(query.page, 10) - 1;
   if (Number.isInteger(parsedPage) && 0 <= parsedPage) {
